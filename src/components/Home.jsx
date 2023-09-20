308b9f017279d61d59ffef42269755da0859f0af
Yhucee14 <uchechukwu7814@gmail.com>
2023-09-20 14:12:34 -0700

    Navbar responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,6 +24,7 @@ const Home = () => {
   const { logoutUser, user } = useUserContext(); // Access the logoutUser function from context
   // const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [loading, setLoading] = useState(true);
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false); // Track mobile navigation state
 
   // Filter images based on the search query
   const filteredImages = imagesData.filter((image) =>
@@ -48,47 +49,77 @@ const Home = () => {
     return <div>Loading...</div>;
   }
 
+  const toggleMobileNav = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
+
   return (
     <div className="py-3 px-2">
-      <div className="flex flex-row justify-between px-4">
+    <nav className="flex flex-col sm:flex-row justify-between items-center px-4">
+      <div className="flex items-center justify-between">
         <h1 className="flex py-3 text-green-600 font-bold text-xl">DnD Gallery</h1>
-
-        <input
-          type="text"
-          placeholder="Search images"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="w-[60%] h-[40px] mt-2 px-2 py-1 rounded-lg bg-transparent border text-black focus:outline-none focus:bg-transparent focus:ring-1 focus:ring-green-600 placeholder-black"
-        />
-
         <button
-          onClick={handleLogout}
-          className="bg-green-600 mt-2 text-white font-bold hover:border-2 hover:border-black px-3 h-[42px] py-1 rounded-xl"
+          className="text-green-600 ml-36 text-2xl sm:hidden"
+          onClick={toggleMobileNav}
         >
-          Logout
+          ☰
         </button>
       </div>
 
+      {isMobileNavOpen ? ( // Conditionally render mobile navigation
+        <div className="sm:hidden flex flex-col items-start">
+          <input
+            type="text"
+            placeholder="Search images"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="w-[90%] h-[40px] mt-2 px-2 py-1 rounded-lg bg-transparent border text-black focus:outline-none focus:bg-transparent focus:ring-1 focus:ring-green-600 placeholder-black"
+          />
+          <button
+            onClick={handleLogout}
+            className="bg-green-600 mt-2 text-white font-bold hover:border-2 hover:border-black px-3 h-[42px] py-1 rounded-xl"
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div className="hidden sm:flex space-x-2">
+          <input
+            type="text"
+            placeholder="Search images"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="w-[60%] h-[40px] px-2 py-1 rounded-lg bg-transparent border text-black focus:outline-none focus:bg-transparent focus:ring-1 focus:ring-green-600 placeholder-black"
+          />
+          <button
+            onClick={handleLogout}
+            className="bg-green-600 mt-2 text-white font-bold hover:border-2 hover:border-black px-3 h-[42px] py-1 rounded-xl"
+          >
+            Logout
+          </button>
+        </div>
+      )}
+    </nav>
 
-
-      <div className="grid sm:grid-cols-3 xx:grid-cols-2 gap-4 p-4 cursor-move ">
-        {filteredImages.map((image) => (
-          <Draggable key={image.id} >
-            <div className="relative sm:w-full sm:h-[320px] xx:w-[150px]  ">
-              <img
-                src={image.src}
-                alt={image.tag}
-                className="w-full h-[320px] rounded-lg"
-              />
-              <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-lg font-semibold opacity-0 hover:opacity-100 transition-opacity">
-                {image.tag}
-              </div>
+    <div className="grid sm:grid-cols-3 xx:grid-cols-2 gap-4 p-4 cursor-move">
+      {filteredImages.map((image) => (
+        <Draggable key={image.id}>
+          <div className="relative sm:w-full sm:h-[320px] xx:w-[150px]">
+            <img
+              src={image.src}
+              alt={image.tag}
+              className="w-full h-[320px] rounded-lg"
+            />
+            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-lg font-semibold opacity-0 hover:opacity-100 transition-opacity">
+              {image.tag}
             </div>
-          </Draggable>
-        ))}
-      </div>
+          </div>
+        </Draggable>
+      ))}
     </div>
-  );
+  </div>
+);
 };
+
 
 export default Home;
